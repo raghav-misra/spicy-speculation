bed6580e30dee754fde614e6aa1f71dac6b8dec1
LehuyH <44150458+LehuyH@users.noreply.github.com>
2022-06-27 16:25:21 -0700

    Improve Hub perf

--- engine/scenes/hub/index.ts
+++ engine/scenes/hub/index.ts
@@ -1,20 +1,27 @@
+import Player from "~~/engine/objects/player"
+
 export default class Hub extends Phaser.Scene{
+    player:Player;
+
     create(){
         this.buildLayers()
+        this.cameras.main.roundPixels = true
+        this.player = new Player(this)
     }
 
     buildLayers(){
         const map = this.make.tilemap({key: 'hubMap'})
-        const buildingsSet = map.addTilesetImage('CL_Buildings','buildingsTiles',undefined,undefined,1,2)
-        const islandSet = map.addTilesetImage('CL_MainLev','islandTiles',undefined,undefined,1,2)
-        
+        const buildingsSet = map.addTilesetImage('CL_Buildings','buildingsTiles',undefined,undefined)
+        const islandSet = map.addTilesetImage('CL_MainLev','islandTiles',undefined,undefined)
+
         map.createLayer("ISLAND_1", islandSet, 0, 0)
         map.createLayer("ISLAND_2", islandSet, 0, 0)
-        map.createLayer("ISLAND_3", islandSet, 0, 0)
-
-        map.createLayer("OCEAN", islandSet, 0, 0)
 
         map.createLayer("ISLAND_BUILDINGS_1", buildingsSet, 0, 0)
         map.createLayer("ISLAND_BUILDINGS_2", buildingsSet, 0, 0)
+    }
+
+    update(time: number, delta: number): void {
+        this.player.update(time,delta)
     }
 }
