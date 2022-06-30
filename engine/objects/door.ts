d78664a36f4971734a108744d62d180ab52180a1
LehuyH <44150458+LehuyH@users.noreply.github.com>
2022-06-30 15:29:03 -0700

    Add door object

---
+++ engine/objects/door.ts
@@ -0,0 +1,29 @@
+interface DoorConfig{
+    scene:Phaser.Scene,
+    x:number,
+    y:number,
+    width:number,
+    height:number,
+    to:string
+}
+
+
+export default class Door extends Phaser.GameObjects.Rectangle{
+    constructor({scene,x,y,width,height,to}:DoorConfig){
+        super(scene,x,y,width,height,0xffffff,0)
+        this.scene.add.existing(this)
+        this.scene.matter.add.gameObject(this,scene.matter.bodies.rectangle(x,y,width,height,{
+            isSensor: true
+        }))
+        
+
+        //Collision events
+        const body = this.body as MatterJS.BodyType
+        //Detect if player is near
+        body.onCollideCallback = (event:any) => {
+            if(event.bodyA.label === `Player`){
+                this.scene.scene.switch(to)
+            }
+        }
+    }
+}
